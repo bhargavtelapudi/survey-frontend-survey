8d6f0e1f65076b84ade870bec9fd35c9253aa93c
bhargavtelapudi <62876599+bhargav443@users.noreply.github.com>
2022-07-06 06:37:58 -0500

    BUG FIX

--- src/services/AuthDataService.js
+++ src/services/AuthDataService.js
@@ -1,14 +1,12 @@
 import http from "../http-common";
-const accessToken = sessionStorage.getItem("authToken");
-
 class AuthService {
   login(authData) {
     return http.post(`/auth/login`, authData);
   }
   logout(userId) {
-    return http.get(`/auth/logout/${userId}`,{
+    return http.get(`/auth/logout/${userId}`, {
       headers: {
-        'x-access-token': accessToken
+        'x-access-token': sessionStorage.getItem("authToken")
       }
     });
   }
--- src/services/SurveyDataService.js
+++ src/services/SurveyDataService.js
@@ -1,25 +1,23 @@
 import http from "../http-common";
-const accessToken = sessionStorage.getItem("authToken");
-
 class SurveyDataService {
   getAllAdminSurveys() {
     return http.get("/survey/list", {
       headers: {
-        'x-access-token': accessToken
+        'x-access-token': sessionStorage.getItem("authToken")
       }
     });
   }
   createSurvey(surveyData) {
     return http.post("/create/survey", surveyData, {
       headers: {
-        'x-access-token': accessToken
+        'x-access-token': sessionStorage.getItem("authToken")
       }
     });
   }
   deleteSurvey(id) {
     return http.delete(`/survey/${id}`, {
       headers: {
-        'x-access-token': accessToken
+        'x-access-token': sessionStorage.getItem("authToken")
       }
     });
   }
